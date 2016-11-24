23e252d4e60ae23765ee4f477934597e3eb45190
Haitham T. Abdulkadier (HalabjaIT) <hiethem.hiethem@yahoo.com>
2016-11-24 10:37:04 +0300

    First

---
+++ Network.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+
+namespace HNetwork
+{
+    public class Network
+    {
+        public string hostName = Dns.GetHostName();
+        public Network()
+        {
+
+        }
+        public IPAddress getCurrentIP()
+        {
+            foreach (IPAddress ip in Dns.GetHostEntry(hostName).AddressList)
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    return ip;
+                }
+            }
+            return new IPAddress(new byte[] { 0, 0, 0, 0 }); ;
+        }
+        public NetworkInterface[] GetAllNetworkInterfaces()
+        {
+            return NetworkInterface.GetAllNetworkInterfaces();
+        }
+        public bool changeIP(NetworkInterface Adapter, IPAddress IP, IPAddress subnetMask, IPAddress defaultGateway,params IPAddress[] dnsServers)
+        {
+            Process tempProcess = new Process();
+            tempProcess.StartInfo = new ProcessStartInfo("netsh", string.Format("interface ip set address name=\"{0}\" static {1} {2} {3}", Adapter.Name, IP, subnetMask, defaultGateway));
+            tempProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            tempProcess.StartInfo.UseShellExecute = true;
+            tempProcess.StartInfo.CreateNoWindow = true;
+            tempProcess.Start();
+            tempProcess.StartInfo = new ProcessStartInfo("netsh", string.Format("interface ip set dns \"{0}\" static 8.8.8.8", Adapter.Name));
+            tempProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            tempProcess.StartInfo.UseShellExecute = true;
+            tempProcess.StartInfo.CreateNoWindow = true;
+            tempProcess.Start();
+            return true;
+        }
+        
+    }
+}
---
+++ SpeedTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HNetwork
+{
+    public class SpeedTest
+    {
+        public SpeedTest()
+        {
+
+        }
+        public int ping(IPAddress IP,int loop = 1,int delay = 1000)
+        {
+            Ping p = new Ping();
+            PingReply pr;
+            long sum = 0;
+            for (int i = 0; i < loop; i++)
+            {
+                pr = p.Send(IP);
+                if (pr.Status == IPStatus.Success)
+                {
+                    sum += pr.RoundtripTime;
+                }
+                Thread.Sleep(delay);
+            }
+            return (int)(sum/loop);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns>Speed in KiloBytes</returns>
+        public double download(Uri file)
+        {
+            WebClient wc = new WebClient();
+            double begin = Environment.TickCount;
+            wc.DownloadFile(file, @"d:\t" + begin);
+            double end = Environment.TickCount;
+            double fileSizeInByte = new FileInfo(@"d:\t" + begin).Length;
+            File.Delete(@"d:\t" + begin);
+            return (fileSizeInByte / ((end - begin) / 1000) / 1024);
+        }
+    }
+}
